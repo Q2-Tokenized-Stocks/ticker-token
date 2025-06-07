e7fb232b9bcc18a0de5f43a4abb7b350f6f0e6e6
Artur <flkr.noise@Arturs-MacBook-Air.local>
2025-06-07 12:56:20 +0300

    admin methods w test

--- programs/ticker-token/src/lib.rs
+++ programs/ticker-token/src/lib.rs
@@ -1,16 +1,173 @@
 use anchor_lang::prelude::*;
+use anchor_lang::prelude::Pubkey;
+use anchor_spl::token_interface::{initialize_mint2, InitializeMint2};
+use anchor_spl::token::Token;
 
 declare_id!("8mPWhPVTKG4zXp5JFqsxA5ZMNhUqWThz5MJjrQS4VB4Z");
+
+const MAX_ADMINS: usize = 100;
+
+#[account]
+pub struct Registry {
+    pub super_admin: Pubkey,
+    pub admins: Vec<Pubkey>,
+}
+
+#[account]
+pub struct TickerData {
+    pub ticker: [u8; 8],
+	pub decimals: u8,
+    pub mint: Pubkey,
+}
+
+#[derive(Accounts)]
+pub struct InitContext<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [b"registry"],
+        bump,
+        payer = payer,
+        space = 8					// Anchor-дескриптор (дисриминатор, нужен всегда)
+              + 32                  // super_admin: Pubkey
+              + 4                   // длина вектора admins (u32, 4 байта)
+              + 32 * MAX_ADMINS     // каждый админ по 32 байта
+    )]
+    pub registry: Account<'info, Registry>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AdminContext<'info> {
+    #[account(mut, signer)]
+    pub caller: Signer<'info>,
+
+    #[account(mut, seeds = [b"registry"], bump)]
+    pub registry: Account<'info, Registry>,
+}
+
+#[derive(Accounts)]
+#[instruction(ticker: String, decimals: u8)]
+pub struct TickerContext<'info> {
+    #[account(mut)]
+    pub caller: Signer<'info>,
+
+    #[account(
+        seeds = [b"registry"],
+        bump,
+        constraint = registry.admins.contains(&caller.key()) @ TickerError::Unauthorized
+    )]
+    pub registry: Account<'info, Registry>,
+
+    #[account(
+        init,
+        seeds = [b"ticker", ticker.as_bytes()],
+        bump,
+        payer = caller,
+        space = 8 	// anchor header
+			  + 8 	// ticker
+			  + 32 	// mint
+			  + 1 	// decimals
+    )]
+    pub ticker_data: Account<'info, TickerData>,
+
+    /// CHECK: manually handled mint account
+    #[account(mut)]
+    pub mint: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[error_code]
+pub enum TickerError {
+    #[msg("Unauthorized")]
+    Unauthorized,
+    #[msg("Ticker name too long")]
+    TickerTooLong,
+    #[msg("Too many admins")]
+    TooManyAdmins,
+	#[msg("Admin not found")]
+	AdminNotFound,
+}
 
 #[program]
 pub mod ticker_token {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init(
+        ctx: Context<InitContext>
+    ) -> Result<()> {
+        let admin = ctx.accounts.payer.key();
+        let registry = &mut ctx.accounts.registry;
+
+        registry.super_admin = ctx.accounts.payer.key();
+        registry.admins = vec![admin];
+
+        Ok(())
+    }
+
+    pub fn add_admin(ctx: Context<AdminContext>, new_admin: Pubkey) -> Result<()> {
+        let registry = &mut ctx.accounts.registry;
+
+        assert_super_admin(registry, &ctx.accounts.caller.key())?;
+
+        require!(registry.admins.len() < MAX_ADMINS, TickerError::TooManyAdmins);
+        registry.admins.push(new_admin);
+
+        Ok(())
+    }
+
+	pub fn remove_admin(ctx: Context<AdminContext>, target: Pubkey) -> Result<()> {
+		let registry = &mut ctx.accounts.registry;
+
+		assert_super_admin(registry, &ctx.accounts.caller.key())?;
+
+		let index = registry.admins.iter().position(|a| *a == target)
+			.ok_or(TickerError::AdminNotFound)?;
+
+		registry.admins.remove(index);
+		Ok(())
+	}
+
+    pub fn create_ticker(ctx: Context<TickerContext>, ticker: String, decimals: u8) -> Result<()> {
+        require!(ticker.len() <= 8, TickerError::TickerTooLong);
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            InitializeMint2 {
+                mint: ctx.accounts.mint.to_account_info(),
+            },
+        );
+
+        initialize_mint2(
+            cpi_ctx,
+            decimals,
+            &ctx.accounts.caller.key(),
+            Some(&ctx.accounts.caller.key()),
+        )?;
+
+        let mut fixed_ticker = [0u8; 8];
+        fixed_ticker[..ticker.len()].copy_from_slice(ticker.as_bytes());
+
+        let data = &mut ctx.accounts.ticker_data;
+
+        data.ticker = fixed_ticker;
+        data.mint = ctx.accounts.mint.key();
+		data.decimals = decimals;
+
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+fn assert_super_admin(registry: &Registry, caller: &Pubkey) -> Result<()> {
+    require!(
+        *caller == registry.super_admin,
+        TickerError::Unauthorized
+    );
+
+    Ok(())
+}
--- tests/ticker-token.ts
+++ tests/ticker-token.ts
@@ -1,15 +1,134 @@
+import { test, before } from 'node:test'
+import assert from 'node:assert/strict'
+
 import * as anchor from '@coral-xyz/anchor'
 import { Program } from '@coral-xyz/anchor'
+
+import { PublicKey, SystemProgram, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js'
+
 import { TickerToken } from '~/target/types/ticker_token'
 
-import test from 'node:test'
-import assert from 'node:assert'
-
 anchor.setProvider(anchor.AnchorProvider.env())
-const program = anchor.workspace.tickerToken as Program<TickerToken>
 
-test('Is initialized!', async () => {
-	const tx = await program.methods.initialize().rpc()
-	console.log('TX:', tx)
-	assert.ok(tx)
+const provider = anchor.getProvider()
+const program = anchor.workspace.ticker_token as Program<TickerToken>
+
+const registryPDA = () => PublicKey.findProgramAddressSync(
+    [Buffer.from('registry')],
+    program.programId
+)
+
+//const findTickerPda = (ticker : string) =>
+//	PublicKey.findProgramAddressSync(
+//		[Buffer.from('ticker'), Buffer.from(ticker)], 
+//		program.programId
+//	)
+
+before(async () => {
+	const [registry] = registryPDA()
+  	
+	const instance = await program.methods
+		.init()
+		.accounts({
+			payer: provider.wallet.publicKey,
+			// @ts-ignore
+			registry,
+			systemProgram: SystemProgram.programId
+		})
+		.rpc()
+})
+
+test('registry was initialized correctly', async () => {
+  const [registry] = registryPDA()
+  const account = await program.account.registry.fetch(registry)
+
+  assert.equal(
+	account.superAdmin.toBase58(), provider.wallet.publicKey.toBase58(), 
+  	'Super admin should be the wallet used to initialize the registry'
+  )
+})
+
+test('init fails if registry already initialized', async () => {
+	const [registry] = registryPDA()
+
+	await assert.rejects(async () => {
+		await program.methods.init().accounts({
+    		payer: provider.wallet.publicKey,
+			// @ts-ignore
+    		registry,
+    		systemProgram: SystemProgram.programId
+    	}).rpc()
+ 	})
+})
+
+test('super admin can add a new admin', async () => {
+	const [registry] = registryPDA()
+	const admin = Keypair.generate()
+
+	await program.methods.addAdmin(admin.publicKey).accounts({
+    	caller: provider.wallet.publicKey,
+    	// @ts-ignore
+    	registry
+  	}).rpc()
+
+	const { admins } = await program.account.registry.fetch(registry)
+
+  	assert.ok(
+    	admins.some(a => a.toBase58() === admin.publicKey.toBase58()),
+    	'New admin should be added to the registry'
+  	)
+})
+
+test('non-admin cannot add new admin', async () => {
+	const [registry] = registryPDA()
+	const admin = Keypair.generate()
+	const nonadmin = Keypair.generate()
+
+	await assert.rejects(
+    	program.methods.addAdmin(admin.publicKey).accounts({
+    		caller: nonadmin.publicKey,
+	      	// @ts-ignore
+    	  	registry
+    	}).signers([nonadmin]).rpc()
+	)
+})
+
+test('admin can be removed', async () => {
+	const [registry] = registryPDA()
+	const adminToRemove = Keypair.generate()
+
+	// Add admin first
+	await program.methods.addAdmin(adminToRemove.publicKey).accounts({
+    	caller: provider.wallet.publicKey,
+		// @ts-ignore
+		registry
+	}).rpc()
+
+	// Then remove
+	await program.methods.removeAdmin(adminToRemove.publicKey).accounts({
+    	caller: provider.wallet.publicKey,
+    	// @ts-ignore
+    	registry
+	}).rpc()
+
+	const { admins } = await program.account.registry.fetch(registry)
+
+  	assert.ok(
+    	!admins.some(admin => admin.toBase58() === adminToRemove.publicKey.toBase58()),
+    	'Admin should be removed from the registry'
+  	)
+})
+
+test('non-admin cannot remove admin', async () => {
+	const [registry] = registryPDA()
+	const nonadmin = Keypair.generate()
+	const target = Keypair.generate()
+
+	await assert.rejects(
+    	program.methods.removeAdmin(target.publicKey).accounts({
+      		caller: nonadmin.publicKey,
+      		// @ts-ignore
+      		registry
+    	}).signers([nonadmin]).rpc()
+	)
 })
